1d53f7b8f97105acb3df3da72c11ef62e830f38e
akarsu35 <cumhurakarsu@gmail.com>
2024-06-19 15:29:30 +0300

    background

--- src/components/Healy.jsx
+++ src/components/Healy.jsx
@@ -8,22 +8,20 @@ import {
 } from '@material-tailwind/react'
 export default function Healy({ id, name, img }) {
   return (
-    <Card className="flex p-2 md:p-0 justify-center items-center mt-6 w-[21rem] hover:shadow-2xl">
-      <CardHeader color="blue-gray" className="relative">
-        <img
-          src={img}
-          alt={name}
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          {name}
-        </Typography>
-        
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button>ÜRÜNÜ İNCELE</Button>
-      </CardFooter>
-    </Card>
+    <div className="bg-[url('./images/iphone.png')] flex justify-center items-center bg-cover bg-center w-[21rem] md:w-[40rem] h-[600px]">
+      <Card className="flex p-2 md:p-0 justify-center items-center w-[14.5rem] hover:shadow-2xl">
+        <CardHeader color="blue-gray" className="relative">
+          <img src={img} alt={name} />
+        </CardHeader>
+        <CardBody>
+          <Typography variant="h5" color="blue-gray" className="mb-2">
+            {name}
+          </Typography>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button>ÜRÜNÜ İNCELE</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,14 @@ import YouTubeEmbed from './YoutubeEmbed'
 
 export default function Home() {
   return (
-    <div className='flex justify-center'>
+    <div className="flex justify-center">
+      
       <div className="w-[90%] min-h-[600px] mt-4 flex flex-col md:flex-row">
         <div className="flex p-4 w-[20rem] md:w-[60rem] items-center justify-center">
           {/* <YouTubeEmbed embedId="dQw4w9WgXcQ" /> */}
           <HealyDeviceInfo />
         </div>
-        <div className="flex p-4 md:mr-2 items-center justify-center w-[20rem] md:w-[40rem] h-[650px]">
+        <div className="flex p-4 md:mr-2 items-center justify-center bg-[#B86BA0] bg-opacity-90 w-[20rem] md:w-[40rem] h-[650px] rounded-xl">
           <InstagramEmbed url="https://www.instagram.com/reel/C55uy34tOQ8/?igsh=MThwbzM1OXdjZG85ag==" />
         </div>
       </div>
