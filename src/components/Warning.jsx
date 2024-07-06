4884d7fc8049a0e1689cb6ba5f3cc6470a6de56f
akarsu35 <cumhurakarsu@gmail.com>
2024-07-07 01:01:40 +0300

    footer update

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -1,7 +1,7 @@
 export default function Warning() {
   return (
     <div>
-      <h2>
+      <h2 className="text-[7px] md:text-[10px]">
         Uyarı: Geleneksel tıp, cihazın etkinliğini kabul
         etmemektedir. Kullanımı kişisel tercihe bağlıdır ve tıbbi tedavinin
         yerini almamalıdır. Her zaman bir sağlık profesyoneline danışınız. Bu
