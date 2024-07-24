a105d3888aa7912c09de16044fdd8a1dc706c361
akarsu35 <cumhurakarsu@gmail.com>
2024-07-24 15:33:44 +0300

    seo1

--- sitemap-builder.cjs
+++ sitemap-builder.cjs
@@ -1,41 +1,10 @@
 // sitemap-builder.cjs
-const sitemap = require('sitemap')
-const { Readable } = require('stream')
-const path = require('path')
-const fs = require('fs')
+import { createSitemap } from 'react-router-sitemap-maker'
+import routes from './routes' // React Router route'larınızı içeren dosya
 
-// Burada kullanmak için bir rotalar dizisi oluşturun
-const links = [
-  { url: '/', changefreq: 'daily', priority: 0.9 },
-  { url: '/healy-gold-edition', changefreq: 'weekly', priority: 0.7 },
-  { url: '/healy-holistic-edition', changefreq: 'weekly', priority: 0.7 },
-  { url: '/healy-resonance-edition', changefreq: 'weekly', priority: 0.7 },
-  { url: '/healy-resonance-plus-edition', changefreq: 'weekly', priority: 0.7 },
-  { url: '/healy-professional-edition', changefreq: 'weekly', priority: 0.7 },
-  { url: '/maghealy-classic-edition', changefreq: 'weekly', priority: 0.7 },
-  {
-    url: '/maghealy-professional-edition',
-    changefreq: 'weekly',
-    priority: 0.7,
-  },
-  { url: '/healy-coil', changefreq: 'weekly', priority: 0.7 },
-  { url: '/hakkımızda', changefreq: 'monthly', priority: 0.5 },
-  { url: '/healy-nedir?', changefreq: 'monthly', priority: 0.5 },
-  { url: '/kampanyalar', changefreq: 'monthly', priority: 0.5 },
-  { url: '/iletişim', changefreq: 'monthly', priority: 0.5 },
-]
+const sitemap = createSitemap({
+  baseUrl: 'https://www.healyhayat.com.tr',
+  // Eğer isterseniz, burada route'lara özel öncelikler veya diğer sitemap özellikleri ekleyebilirsiniz
+})(routes)
 
-const generateSitemap = () => {
-  // SitemapStream yerine sitemap.createSitemap kullanılır
-  const sitemapContent = sitemap.createSitemap({
-    hostname: 'https://www.healyhayat.com.tr',
-    urls: links,
-  })
-
-  fs.writeFileSync(
-    path.resolve('./public', 'sitemap.xml'),
-    sitemapContent.toString()
-  )
-}
-
-generateSitemap()
+sitemap.save('./public/sitemap.xml')
