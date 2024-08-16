9833ae19b20695ab8242a3e4763060bd81d3f88d
akarsu35 <cumhurakarsu@gmail.com>
2024-08-16 11:04:36 +0300

    bolg update

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function UnderConstruction() {
   return (
-    <div className="flex items-center justify-center bg-notpage bg-cover bg-center-bottom min-h-[66vh]">
+    <div className="flex items-center justify-center min-h-[66vh]">
       <div className="text-center p-6 rounded-lg shadow-lg flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold text-gray-100 mb-4">
+        <h1 className="text-4xl font-bold text-gray-700 mb-4">
           Bu Sayfa Yapım Aşamasında
         </h1>
-        <p className="text-gray-300 mb-6 ">Çok kısa sürede sizinle olacağız.</p>
+        <p className="text-gray-600 mb-6 ">Çok kısa sürede içeriklerimizle sizinle olacağız.</p>
         <div className="bg-[#B756A0] w-40 h-8 rounded-md flex justify-center items-center hover:bg-[#5297C8] transition duration-500">
           <a href="/" className="text-white font-semibold">
             Ana Sayfaya Dön
